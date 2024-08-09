f9d700fe6310176a437fb5d279650b4f30bcc9ae
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-08-09 13:38:09 +0530

    reverted Revert "Updated Gemini.js"
    This reverts commit b5d2778f17727da33524947c7031d8d963e713ae.

--- Dailo_AI/src/config/Gemini.js
+++ Dailo_AI/src/config/Gemini.js
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = process.env.APIKEY;
+const apiKey = "AIzaSyC17nuqXJNiFdO93wtqHncJYJMpD9H48bo";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
